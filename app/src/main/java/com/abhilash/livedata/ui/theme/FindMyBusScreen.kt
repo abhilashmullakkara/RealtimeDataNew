aff786f5f5306f3f54598b275250c1582ba090fb
abhilashmullakkara <abhilssh85@gmail.com>
2023-07-14 22:06:14 +0530

    FindMyBusScreen updated

--- app/src/main/java/com/abhilash/livedata/ui/theme/FindMyBusScreen.kt
+++ app/src/main/java/com/abhilash/livedata/ui/theme/FindMyBusScreen.kt
@@ -1,7 +1,9 @@
 package com.abhilash.livedata.ui.theme
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -12,12 +14,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -26,32 +26,80 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 
-
 @Composable
 fun FindMyBusScreen(navController: NavController) {
     val scroll = rememberScrollState()
-    val children1 = remember { mutableStateListOf<String>() }
+    var firstGeneration: SnapshotStateList<String>
+    var secondGeneration = SnapshotStateList<String>()
+    var thirdGeneration = remember { mutableStateListOf<String>() }
+    val memberCounts = remember { mutableStateMapOf<String, Int>() }
 
-    LaunchedEffect(Unit) {
-        // Retrieve the children names from Firebase
-        val database = FirebaseDatabase.getInstance()
-        val reference = database.getReference("")
 
-        reference.addChildEventListener(object : ChildEventListener {
-            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
-                val childName = snapshot.key
-                childName?.let { name ->
-                    children1.add(name)
-                }
-            }
+//    LaunchedEffect(Unit) {
+//        // Retrieve the first generation names from Firebase
+//        val database = FirebaseDatabase.getInstance()
+//        val reference = database.getReference("")
+//
+//        reference.addChildEventListener(object : ChildEventListener {
+//            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+//                val childName = snapshot.key
+//                childName?.let { name ->
+//                    firstGeneration.add(name)
+//                    memberCounts[name] = 0 // Initialize member count to 0
+//                }
+//            }
+//
+//            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+//            override fun onChildRemoved(snapshot: DataSnapshot) {}
+//            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+//            override fun onCancelled(error: DatabaseError) {}
+//        })
+//    }
 
-            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
-            override fun onChildRemoved(snapshot: DataSnapshot) {}
-            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
-            override fun onCancelled(error: DatabaseError) {}
-        })
-    }
-
+//    LaunchedEffect(firstGeneration) {
+//        firstGeneration.forEach { firstGenName ->
+//            val database = FirebaseDatabase.getInstance()
+//            val reference = database.getReference(firstGenName)
+//
+//            reference.addChildEventListener(object : ChildEventListener {
+//                override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+//                    val childName = snapshot.key
+//                    childName?.let { name ->
+//                        secondGeneration.add(name)
+//                        memberCounts[firstGenName] = memberCounts[firstGenName]?.plus(1) ?: 1
+//                    }
+//                }
+//
+//                override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+//                override fun onChildRemoved(snapshot: DataSnapshot) {}
+//                override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+//                override fun onCancelled(error: DatabaseError) {}
+//            })
+//        }
+//    }
+//
+//    LaunchedEffect(secondGeneration) {
+//        secondGeneration.forEach { secondGenName ->
+//            val firstGenName = secondGenName.substringBefore('/')
+//            val database = FirebaseDatabase.getInstance()
+//            val reference = database.getReference(secondGenName)
+//
+//            reference.addChildEventListener(object : ChildEventListener {
+//                override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+//                    val childName = snapshot.key
+//                    childName?.let { name ->
+//                        thirdGeneration.add(name)
+//                    }
+//                }
+//
+//                override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+//                override fun onChildRemoved(snapshot: DataSnapshot) {}
+//                override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+//                override fun onCancelled(error: DatabaseError) {}
+//            })
+//        }
+//    }
+firstGeneration= findRoot(ref = "") as SnapshotStateList<String>
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -66,19 +114,25 @@ fun FindMyBusScreen(navController: NavController) {
         }
 
         Column {
-            Text("Children Names:")
-            for (i in 0 until children1.size) {
-                val name = children1[i]
+            Text("First Generation:")
+            var name1=""
+            firstGeneration.forEach { name ->
                 Text(name)
-                for (j in 0 until i) {
-                    val childPath = "$children1[i]/"
-                    val secondGeneration: List<String> = childrenListScreen(childPath)
-                    Column {
-                        Text("Children Names:")
-                        secondGeneration.forEach { thirdName ->
-                            Text(thirdName)
-                        }
-                    }
+                name1=name
+                secondGeneration=findRoot(ref = name) as SnapshotStateList<String>
+                //Text("Member Count: ${memberCounts[name]}")
+            }
+            secondGeneration.forEach {
+
+                if (it.isNotBlank() ){
+                    Text(text = it)
+                    thirdGeneration=findRoot(ref = "$name1/+$it") as SnapshotStateList<String>
+                }
+                
+            }
+            thirdGeneration.forEach { 
+                if (it.isNotBlank()){
+                    Text(text = it)
                 }
             }
         }
@@ -87,19 +141,19 @@ fun FindMyBusScreen(navController: NavController) {
 
 
 @Composable
-fun childrenListScreen(child: String): List<String> {
-    val childrenNames = remember { mutableStateListOf<String>() }
-
-    // Retrieve the children names from Firebase
+fun findRoot(ref:String):List<String> {
+    val firstGeneration = remember { mutableStateListOf<String>() }
     LaunchedEffect(Unit) {
+        // Retrieve the first generation names from Firebase
         val database = FirebaseDatabase.getInstance()
-        val reference = database.getReference(child)
+        val reference = database.getReference(ref)
 
         reference.addChildEventListener(object : ChildEventListener {
             override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
                 val childName = snapshot.key
                 childName?.let { name ->
-                    childrenNames.add(name)
+                    firstGeneration.add(name)
+                    //memberCounts[name] = 0 // Initialize member count to 0
                 }
             }
 
@@ -109,8 +163,6 @@ fun childrenListScreen(child: String): List<String> {
             override fun onCancelled(error: DatabaseError) {}
         })
     }
-
-    return childrenNames
+    return firstGeneration
 }
-
 
